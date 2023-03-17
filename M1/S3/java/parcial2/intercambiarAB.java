b48e07a92fa421d7289daf68c523e1374d26505b
EquihuaGarciaPriscilaLucero <126208719+EquihuaGarciaPriscilaLucero@users.noreply.github.com>
2023-03-17 08:15:13 -0600

    Add files via upload

---
+++ M1/S3/java/parcial2/intercambiarAB.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class SaludoApp  
+  {   
+   public static void main(String[] args)  
+     {
+      int a=5,b=3,t=0;
+      t=a;
+      a=b;
+      b=t;
+      System.out.println("El valor de A:" +a);
+      System.out.println("El valor de B:" +b);
+     }
+  }
